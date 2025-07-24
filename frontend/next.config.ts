65cc7e8ccaf052403f8c5a6f862f273c8ed1a477
puntsag <puntsagg0@gmail.com>
2025-07-24 21:43:44 +0800

    zurag zasaw

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
-module.exports = {
-  reactStrictMode: false,
-};
-
 const nextConfig: NextConfig = {
+  reactStrictMode: false,
   images: {
     domains: [
       "pub-52aed96024654efbb4ebd3a54cbfd43f.r2.dev",
