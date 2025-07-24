1313731ba1a4f45d1f2cc18d4811db7318763d27
puntsag <puntsagg0@gmail.com>
2025-07-24 10:08:27 +0800

    backend deploy

--- backend/resolvers/queries/shop/get-shop.ts
+++ backend/resolvers/queries/shop/get-shop.ts
@@ -1,5 +1,5 @@
 import prisma from "@/lib/prisma";
-import { QueryResolvers } from "@/app/types/generated";
+import { QueryResolvers } from "@/types/generated";
 export const shop: QueryResolvers["shop"] = async () => {
   const shops = await prisma.shop.findMany();
   return shops;
